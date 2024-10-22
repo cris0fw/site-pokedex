f9310eef260648d52066d7461d8618ed80f9ba58
cris0fw <crisludue5@gmail.com>
2024-10-22 20:21:57 -0300

    baseurl solucionado

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://cris0fw.github.io/site-pokedex/imagen2.png",
+  base: "https://cris0fw.github.io/site-pokedex",
 });
